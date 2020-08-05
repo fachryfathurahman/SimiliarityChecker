1504dc55cb3a7d8ca3c91e26099f590bf5d3c950
fachryfathurahman <fachry123fathurahman@gmail.com>
2020-08-05 22:25:05 +0700

    tes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
 from similiarity import *
 import requests
+import sys
 
 url = "https://api.ofcode.dev/ilab/submission/"
-TaskDetailID = "63193"
+TaskDetailID = str(sys.argv)
 urlWithID = url + TaskDetailID
 
 responseWithID = requests.get(urlWithID)
@@ -27,25 +28,3 @@ for x in allData['data']:
         last_percentage = [x['TaskDetailID'], resutlt]
 
 print(last_percentage)
-
-# directory = "D:\praktikumsem4\machine learning\similiaritychecker\\testing"
-# test_path = "D:\praktikumsem4\machine learning\similiaritychecker\\testing\\257_1.txt"
-# files_path = [os.path.abspath(os.path.join(directory, p)) for p in os.listdir(directory)]
-# max_value = float()
-# path_max = "not found"
-# list_final = []
-# for files in files_path:
-#     percentage = documentSimiliarity(test_path, files)
-#     split_test_path = test_path.split("_")[0]
-#     split_files = files.split("_")[0]
-#     print(files, percentage)
-#     if test_path == files:
-#         continue
-#     if split_test_path == split_files:
-#         continue
-#     if percentage[1] >= max_value:
-#         path_max = files
-#         max_value = percentage[1]
-#         list_final = [path_max, percentage]
-#
-# print(list_final)

cd2a6270a52f11e82756828d4ab46f8e1832ec01
fachryfathurahman <fachry123fathurahman@gmail.com>
2020-08-18 23:56:26 +0700

    Update similiarity.py

--- similiarity.py
+++ similiarity.py
@@ -1,7 +1,4 @@
 import math
-import numpy as np
-
-
 
 
 def remove_comment(file_string):
@@ -37,20 +34,22 @@ def get_words_from_line_list(text):
     word_list = text.split()
     list_stop_word = ['#include']
     word_list = [x for x in word_list if x not in list_stop_word]
-    # print(word_list)
+
     return word_list
 
 
 def get_line_from_line_list(text):
     word_list = text.split('\n')
+    # todo bagian penting
+    while "" in word_list:
+        word_list.remove("")
     return word_list
 
 
 def count_line_frequency(line_list_1, line_list_2):
-    line_list_1 = set(line_list_1)
-    line_list_2 = set(line_list_2)
-    same_line = line_list_1.intersection(line_list_2)
-    return (2*len(same_line))/(len(line_list_1)+len(line_list_2))
+    # todo bagian penting
+    same_line = list(filter(lambda x: x in line_list_1, line_list_2))
+    return (2 * len(same_line)) / (len(line_list_1) + len(line_list_2))
 
 
 # menghitung frekuensi setiap kata
@@ -89,7 +88,6 @@ def dot_prduct(D1, D2):
     for key in D1:
         if key in D2:
             Sum += (D1[key] * D2[key])
-
     return Sum
 
 
@@ -105,20 +103,51 @@ def vector_angle(D1, D2):
 def line_frequencies_for_file(filename_1, filename_2):
     line_list_1 = filename_1
     separate_list_1 = get_line_from_line_list(line_list_1)
-
     line_list_2 = filename_2
     separate_list_2 = get_line_from_line_list(line_list_2)
     freq_line = count_line_frequency(separate_list_1, separate_list_2)
     return freq_line
 
 
+def line_frequencies_for_fileV2(filename_1, filename_2):
+    line_list_1 = filename_1
+    separate_list_1 = get_words_from_line_list(line_list_1)
+    line_list_2 = filename_2
+    separate_list_2 = get_words_from_line_list(line_list_2)
+    freq_lineV2 = count_word_frequencyV2(separate_list_1, separate_list_2)
+    return freq_lineV2
+
+
+def count_word_frequencyV2(line_list_1, line_list_2):
+    line_list_1 = join_with_n2(line_list_1)
+    line_list_2 = join_with_n2(line_list_2)
+    same_line = list(filter(lambda x: x in line_list_1, line_list_2))
+    return (2 * len(same_line)) / (len(line_list_1) + len(line_list_2))
+
+
+def join_with_n2(line_list):
+    data = list()
+    size = len(line_list)
+    for x in range(size):
+        if x == size - 1:
+            break
+        data.append(str(line_list[x] + " " + line_list[(x + 1)]))
+    return data
+
+
 def documentSimiliarity(filename_1, filename_2):
     sorted_word_list_1 = word_frequencies_for_file(filename_1)
     sorted_word_list_2 = word_frequencies_for_file(filename_2)
     sorted_line_list = line_frequencies_for_file(filename_1, filename_2)
-    sorted_line_list = sorted_line_list*100
+    sorted_line_list = sorted_line_list * 100
+
+    sorted_line_list_n2 = line_frequencies_for_fileV2(filename_1, filename_2) * 100
     distance = vector_angle(sorted_word_list_1, sorted_word_list_2)
-    distance = abs(((np.rad2deg(distance) / 90) * 100)-100)
-    average_percentage = (sorted_line_list+(abs(((np.rad2deg(distance) / 90) * 100) - 100)))/2
-    list = [distance, sorted_line_list]
+    distance = abs(((rad2deg(distance) / 90) * 100) - 100)
+    # average_percentage = (sorted_line_list + (abs(((np.rad2deg(distance) / 90) * 100) - 100))) / 2
+    list = [distance, sorted_line_list, sorted_line_list_n2]
     return list
+
+
+def rad2deg(radian):
+    return radian * (180 / math.pi)
